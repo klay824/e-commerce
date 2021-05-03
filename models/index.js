5b69d41db71fc4b8bcfe3d0b4f094a31358c7125
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-02 21:58:15 -0500

    added foreign keys to Product and Tag tables

--- models/index.js
+++ models/index.js
@@ -18,14 +18,16 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    foreignKey: 'product_id'
   }
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    foreignKey: 'tag_id'
   }
 });
 
